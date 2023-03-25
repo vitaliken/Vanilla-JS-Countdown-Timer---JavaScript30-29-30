e54dc88c6d64701a614ac160e0722d4c771e30f5
Vitaly Hutarau <vitaliken@gmail.com>
2023-03-26 00:43:30 +0300

    init commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let countdown;
+
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+const alarm = document.querySelector('.alarm');
